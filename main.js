37980d30db0e20d4cdee2dd21c35e6607acc566c
Isabel Lee <isabelringing1@gmail.com>
2023-04-06 22:04:02 -0400

    Updated code to be simpler

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ $(document).ready(function() {
             data.sequences[i].levels = levelMap;
             sequences.set(data.sequences[i].name, data.sequences[i]);
         }
-        console.log(sequences);
         addEventListener('mousemove', onMouseMove);
 
         $("#toggle").click(function(){
@@ -35,33 +34,10 @@ $(document).ready(function() {
 });
 
 function setSequence(sequenceName){
-    if (!sequences.has(sequenceName)){
-        console.log("Cannot set sequence " + sequenceName);
-        return;
-    }
     currentSequence = sequences.get(sequenceName);
-    $(".debug").css({"display": "block"});
-    console.log("Current sequence set to " + sequenceName);
 }
 
-function setLevel(levelNum){
-    for (let id of currentObjects.keys()) {
-        $("#" + id)[0].remove();
-    }
-    currentObjects.clear();
-
-    if (currentSequence == null || !currentSequence.levels.has(levelNum)){
-        console.log(levelNum, currentSequence.levels.size)
-        if (levelNum >= currentSequence.levels.size){
-            localStorage.setItem(currentSequence.name, "done");
-            if (currentSequence.nextSequence != null){
-                localStorage.setItem(currentSequence.nextSequence, "unlocked");
-            }
-        }
-        openMenu();
-        return;
-    }
-    
+function setLevel(levelNum){    
     var level = currentSequence.levels.get(levelNum);
     levelFinished = false;
     currentLevel = level;
@@ -77,14 +53,9 @@ function setLevel(levelNum){
 
         var xDelta = 0;
         var yDelta = 0;
-        level.targetX = getPixelValue(level.targetX);
-        level.targetY = getPixelValue(level.targetY);
-        console.log(level.targetX, level.targetY)
         var target = [level.targetX, level.targetY];
         $("#goal")[0].innerHTML = target;
 
-        obj.offsetTargetX = getPixelValue(obj.offsetTargetX);
-        obj.offsetTargetY = getPixelValue(obj.offsetTargetY);
         if (lastMousePos != null){
             xDelta = lastMousePos[0] - level.targetX;
             yDelta = lastMousePos[1] - level.targetY;
@@ -94,27 +65,13 @@ function setLevel(levelNum){
         var yPos = (yDelta * obj.yFollow) + level.targetY + obj.offsetTargetY;
         div.style.top = yPos + "px";
         div.style.left = xPos + "px";
-        var offsetRot = obj.offsetRotation == null ? 0 : obj.offsetRotation;
-        var rot = obj.rotation == null ? 0 : obj.rotation;
-        div.style.rotate = (dist * rot + offsetRot) + "deg";
-        div.style.filter = 'blur(' + (dist * obj.blur / 100) + 'px)';
-        var scaleX = obj.hasOwnProperty("scaleX") ? (1 + (dist * obj.scaleX / 1000)) : 1;
-        var scaleY = obj.hasOwnProperty("scaleY") ? (1 + (dist * obj.scaleY / 1000)) : 1;
-        if (scaleX != 1 || scaleY != 1){
-            div.style.transform = "translate(-50%, -50%) scale(" + scaleX + ", " + scaleY + ")";
-        }
         
         console.log("Setting obj " + obj.name + " to " + xPos + ', ' + yPos);
         div.appendChild(img);
         div.appendChild(glow);
 
-        if ((currentLevel.hasOwnProperty("glow") && currentLevel.glow == "off") || (obj.hasOwnProperty("glow") && obj.glow == "off")){
-            glow.style.transform = "scale(0)";
-        }
-
         div.classList.add("object");
-        var number = obj.number == null ? "" : obj.number;
-        div.id = obj.name + number;
+        div.id = obj.name;
         obj.currentPos = [xPos, yPos];
         currentObjects.set(div.id, obj);
         $("#background").append(div);
@@ -128,13 +85,8 @@ function onMouseMove(e){
 function updateObjects(x, y){
     if (!started){
         lastMousePos = [x, y];
-        if (localStorage.getItem("Intro") == "done"){
-            openMenu();
-        }
-        else{
-            setSequence("Intro");
-            setLevel(1);
-        }
+        setSequence("one");
+        setLevel(1);
         started = true;
         return;
     }
@@ -155,23 +107,10 @@ function updateObjects(x, y){
     for (let [name, obj] of currentObjects) {
         var newLeft = obj.currentPos[0] + xDelta * obj.xFollow;
         var newTop = obj.currentPos[1] + yDelta * obj.yFollow;
-        var offsetRot = obj.offsetRotation == null ? 0 : obj.offsetRotation;
-        var rot = obj.rotation == null ? 0 : obj.rotation;
-        var newRot = (dist * rot + offsetRot) + "deg";
-        var newBlur = 'blur(' + (dist * obj.blur / 100) + 'px)';
 
-        var scaleX = obj.hasOwnProperty("scaleX") ? (1 + (dist * obj.scaleX / 1000)) : 1;
-        var scaleY = obj.hasOwnProperty("scaleY") ? (1 + (dist * obj.scaleY / 1000)) : 1;
-        if (scaleX != 1 || scaleY != 1){
-            document.getElementById(name).style.transform = "translate(-50%, -50%) scale(" + scaleX + ", " + scaleY + ")";
-        }
-        
-        
         $("#" + name).css({
             left: newLeft,
             top: newTop,
-            rotate: newRot,
-            filter: newBlur, '-webkit-filter': newBlur, '-moz-filter': newBlur, '-o-filter': newBlur, '-ms-filter': newBlur,
         });
         obj.currentPos = [newLeft, newTop];
     }
@@ -184,80 +123,7 @@ function updateObjects(x, y){
             levelFinished = true;
             $("#coord")[0].innerHTML = "yay";
             $('.glow').addClass("winState");
-            setTimeout(() => {
-                $('.glow').removeClass("winState")
-                setLevel(currentLevel.level + 1);
-            }, 1000);
-        }
-        if (currentLevel.hasOwnProperty("leeway") && dist < currentLevel.leeway){
-            updateObjects(currentLevel.targetX, currentLevel.targetY);
+
         }
     }
-}
-
-function openMenu(){
-    $("#menu").css({"display": "flex"});
-    $(".debug").css({"display": "none"});
-
-    $("#logo").click(function(){
-        closeMenu();
-        setSequence("Intro");
-        setLevel(1);
-    });
-
-    for (let name of sequences.keys()) {
-        if (document.getElementById(name) != null){
-            var status = localStorage.getItem(name);
-            console.log("Status of " + name + " is "+ status);
-            if (status == null){
-                $("#" + name).attr("class","level locked");
-                $("#" + name).click(null);
-            }
-            else if (status == "unlocked"){
-                $("#" + name).attr("class","level unlocked");
-                $("#" + name).click(function(){
-                    closeMenu();
-                    setSequence(name);
-                    setLevel(1);
-                });
-            }
-            else if (status == "done"){
-                $("#" + name).attr("class","level done");
-                $("#" + name).click(function(){
-                    closeMenu();
-                    setSequence(name);
-                    setLevel(1);
-                });
-            }
-        }   
-    }
-}
-
-function closeMenu(){
-    $("#menu").css({"display": "none"});
-    $(".debug").css({"display": "block"});
-}
-
-function getPixelValue(value){
-    if (!isNaN(value)){
-        return value;
-    }
-    // otherwise it's a (compound) percentage
-    var split = value.split(',');
-    var total = 0;
-    for (var i = 0; i < split.length; i++){
-        var num = Number(split[i].slice(0, -2));
-        var dim = split[i].slice(-1)[0];
-        if (dim == 'w'){
-            total += Math.floor(num / 100 * window.innerWidth);
-        }
-        else if (dim == 'h'){
-            total += Math.floor(num / 100 * window.innerHeight);
-        }
-        else if (dim == 'x'){
-            total += num;
-        }
-    }
-
-    return total;
 }
